dd5221648a7b7dafac896b58d800eff8fb2cc6c3
RG3322 <raghavsankhyaan@gmail.com>
2025-11-01 11:50:52 +0530

    added basic dependencies and file structures

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+
+        id("com.google.devtools.ksp")
+
 }
 
 android {
@@ -57,4 +60,48 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    //ksp
+    ksp("androidx.room:room-compiler:2.8.3")
+
+
+    //open csv
+    implementation("com.github.dokar:kotlin-csv:1.5.0")
+
+    //coil
+
+    implementation("io.coil-kt:coil-compose:2.7.0")
+
+    //compose nav destinations
+    implementation("io.github.raamcosta.compose-destinations:core:2.3.0")
+    ksp("io.github.raamcosta.compose-destinations:ksp:2.3.0")
+
+   //dagger hilt
+     implementation("com.google.dagger:hilt-android:2.57.2")
+     ksp("com.google.dagger:hilt-android-compiler")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    ksp("androidx.hilt:hilt-compiler:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0-alpha01")
+
+
+    //compose dependencies
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.0-alpha01")
+    implementation("androidx.activity:activity-compose:1.7.0-alpha01")
+    implementation("com.google.accompanist:accompanist-systemuicontroller:0.28.0 ")
+
+
+    //Retrofit
+
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.11.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.14")
+
+
+    // room
+    
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+
+
 }
